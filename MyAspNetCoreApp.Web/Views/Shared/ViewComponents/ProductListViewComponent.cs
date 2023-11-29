a38a8153d4e953eda55723eae7fe160b45174215
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-11-29 09:48:23 +0300

    Added View Component

---
+++ MyAspNetCoreApp.Web/Views/Shared/ViewComponents/ProductListViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Web.Models;
+using MyAspNetCoreApp.Web.ViewModels;
+
+namespace MyAspNetCoreApp.Web.Views.Shared.ViewComponents
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _appDbContext;
+        public ProductListViewComponent(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string type = "Default")
+        {
+            var products = _appDbContext.Products
+                .Select(p => new ProductListComponentViewModel()
+                {
+                    Name = p.Name,
+                    Description = p.Description,
+                }).ToList();
+
+            return View(type,products);
+        }
+    }
+}
