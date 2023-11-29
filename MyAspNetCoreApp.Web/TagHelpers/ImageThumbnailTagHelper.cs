6e7d93cf4324a004748998fb8136c773c2f4b086
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-11-29 13:50:11 +0300

    Added Tag Helper

---
+++ MyAspNetCoreApp.Web/TagHelpers/ImageThumbnailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MyAspNetCoreApp.Web.TagHelpers
+{
+    [HtmlTargetElement("thumbnail")]
+    public class ImageThumbnailTagHelper : TagHelper
+    {
+        public string ImageSrc { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //<img src="" />
+
+            output.TagName = "img";
+
+            string fileName = ImageSrc.Split(".")[0]; // noktadan sonrasını böl ve ilk indexi al.
+            string fileExtensions = Path.GetExtension(ImageSrc); // nokta ile beraber uzantıyı aldık.
+
+            output.Attributes.SetAttribute("src", $"{fileName}-100x100{fileExtensions}");
+        }
+    }
+}
