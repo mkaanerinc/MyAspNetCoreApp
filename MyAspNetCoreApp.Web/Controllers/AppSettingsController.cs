cb174c0c09d43744cfd58b5367072db2cc1bb46c
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2023-12-04 14:22:38 +0300

    Added appsettings.json & cookie

---
+++ MyAspNetCoreApp.Web/Controllers/AppSettingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class AppSettingsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppSettingsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.SmsKey = _configuration["Keys:Sms"];
+            ViewBag.EmailKey = _configuration.GetSection("Keys")["Email"];
+            return View();
+        }
+    }
+}
---
+++ MyAspNetCoreApp.Web/Controllers/CookieController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAspNetCoreApp.Web.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Response.Cookies.Append("background-color", "red", new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(2)
+            }); ;
+
+            return View();
+        }
+
+        public IActionResult CookieRead()
+        {
+            var bgColor = HttpContext.Request.Cookies["background-color"];
+
+            ViewBag.BgColor = bgColor;
+
+            return View();
+        }
+    }
+}
